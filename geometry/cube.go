5444d8a1f1d3475e997736787440a490b981e752
piyush175 <piyush1751995@gmail.com>
2022-08-15 17:50:44 +0530

    Zero edge error

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,5 +1,10 @@
 package geometry
 
-func CubeVol(n int) int {
-	return n * n * n
+import "errors"
+
+func CubeVol(n int) (int, error) {
+	if n != 0 {
+		return n * n * n, nil
+	}
+	return 0, errors.New("zero edge not allowed")
 }
